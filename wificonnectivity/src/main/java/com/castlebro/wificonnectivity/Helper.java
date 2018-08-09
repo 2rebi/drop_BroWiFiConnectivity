8f815795e483cc0873c8d16f69d9cba68ee69408
Lee Jaeseong <fg9658@gmail.com>
2018-08-09 15:27:14 +0900

    문법 에러 픽스.

--- wificonnectivity/src/main/java/com/castlebro/wificonnectivity/Helper.java
+++ wificonnectivity/src/main/java/com/castlebro/wificonnectivity/Helper.java
@@ -4,27 +4,27 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.SupplicantState;
 
-import static com.castlebro.wificonnectivity.WiFiConnectivity.WIFI_STATE_UNSPECIFIED;
-import static com.castlebro.wificonnectivity.WiFiConnectivity.WIFI_DISCONNECTED;
-import static com.castlebro.wificonnectivity.WiFiConnectivity.WIFI_CONNECTING;
-import static com.castlebro.wificonnectivity.WiFiConnectivity.WIFI_CONNECTED;
+import static com.castlebro.wificonnectivity.WiFiConnectionState.CONNECTED;
+import static com.castlebro.wificonnectivity.WiFiConnectionState.CONNECTING;
+import static com.castlebro.wificonnectivity.WiFiConnectionState.DISCONNECTED;
+import static com.castlebro.wificonnectivity.WiFiConnectionState.NONE;
 
 class Helper {
 
     static WiFiConnectionState convertWifiConnection(NetworkInfo nInfo, SupplicantState sState) {
         if ((nInfo != null) && (nInfo.getType() == ConnectivityManager.TYPE_WIFI)) {
             NetworkInfo.State nState = nInfo.getState();
-            if (NetworkInfo.State.DISCONNECTED == nState) return WiFiConnectionState.WIFI_DISCONNECTED;
-            if (NetworkInfo.State.CONNECTING == nState) return WiFiConnectionState.WIFI_CONNECTING;
-            if (NetworkInfo.State.CONNECTED == nState) return WiFiConnectionState.WIFI_CONNECTED;
+            if (NetworkInfo.State.DISCONNECTED == nState) return DISCONNECTED;
+            if (NetworkInfo.State.CONNECTING == nState) return CONNECTING;
+            if (NetworkInfo.State.CONNECTED == nState) return CONNECTED;
         }
         if (sState != null) {
-            if (SupplicantState.DISCONNECTED == sState) return WiFiConnectionState.WIFI_DISCONNECTED;
-            if (SupplicantState.ASSOCIATING == sState) return WiFiConnectionState.WIFI_CONNECTING;
-            if (SupplicantState.AUTHENTICATING == sState) return WiFiConnectionState.WIFI_CONNECTING;
-            if (SupplicantState.COMPLETED == sState) return WiFiConnectionState.WIFI_CONNECTED;
-            if (SupplicantState.ASSOCIATED == sState) return WiFiConnectionState.WIFI_CONNECTED;
+            if (SupplicantState.DISCONNECTED == sState) return DISCONNECTED;
+            if (SupplicantState.ASSOCIATING == sState) return CONNECTING;
+            if (SupplicantState.AUTHENTICATING == sState) return CONNECTING;
+            if (SupplicantState.COMPLETED == sState) return CONNECTED;
+            if (SupplicantState.ASSOCIATED == sState) return CONNECTED;
         }
-        return WiFiConnectionState.WIFI_STATE_UNSPECIFIED;
+        return NONE;
     }
 }
